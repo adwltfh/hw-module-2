777f5abb32f48de053bc102c8678e2d4aad1317a
AdawiyyahLatifah <adawiyyahlatifah@gmail.com>
2022-03-23 13:02:31 +0700

    adding module2 hw

---
+++ src/Album.js
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// class Album extends React.Component {
+//     render() {
+//         return(
+//             <div className="album-container">
+//             <div className="album-cover">
+//                 <img 
+//                 className="cover-img"
+//                 src={this.props.cover}
+//                 />
+//             </div>
+//             <div className="song-title">
+//                 <h4>{this.props.title}</h4>
+//             </div>
+//             <div className="song-artist">
+//                 <p>{this.props.artists}</p>
+//             </div>
+//             <a href={this.props.url}><button className="btn">Select</button></a>
+//             </div>
+//         );
+//     }
+ 
+// }
+
+// export default Album;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import data from './data/data';
+// import Album from './home/src/Album';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="album-container">
+      <div className="album-cover">
+        <img 
+          className="cover-img"
+          src={data.album.images[0].url}
+        />
+      </div>
+      <div className="song-title">
+        <h4>{data.album.name}</h4>
+      </div>
+      <div className="song-artist">
+        <p>{data.artists[0].name}</p>
+      </div>
+      <a href={data.album.artists[0].external_urls.spotify}><button className="btn">Select</button></a>
     </div>
-  );
+  )
 }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className='albums'>
+//           {data.map((dataItem) => {
+//             return (
+//               <Album
+//                 cover = {dataItem.album.images[0].url}
+//                 title = {dataItem.album.name}
+//                 artists = {dataItem.artists[0].name}
+//                 url = {dataItem.album.artists[0].external_urls.spotify}
+//               />
+//             )
+//           })}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
