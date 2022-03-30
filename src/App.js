2691c5279d28b0f576b5e70112bbc4940d122d96
AdawiyyahLatifah <adawiyyahlatifah@gmail.com>
2022-03-30 14:21:06 +0700

    adding hw1 module 3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Albums from './pages/album';
 import SpotifyPlaylist from './pages/playlist';
+import SearchBar from './pages/search-bar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar/>
       <Albums />
       <hr/>
       <SpotifyPlaylist />
