a8cc97d4c281568381c744e5dad949b8c4fd99e8
AdawiyyahLatifah <adawiyyahlatifah@gmail.com>
2022-04-20 10:08:26 +0700

    revise

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -101,15 +101,4 @@ const AppNavBar: React.FC = ({children}) => {
         </Container>
     );
 };
-    
-// <section className="nav">
-//     <div className="nav-container">
-//         <div className="nav-logo">
-//             <h2>Dawtify</h2>
-//         </div>
-//         <nav>
-//             {children}
-//         </nav>
-//     </div>
-// </section>
 export default AppNavBar;
